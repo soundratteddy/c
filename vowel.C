ba5bc40b0d2dab60509527937a63e16d78cf9a88
ANANDKUMAR594 <anandkumar.m594@gmail.com>
2017-08-13 08:56:11 +0530

    vowel or not

---
+++ vowel.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+void main()
+{
+    char ch;
+ 
+    printf("Enter an alphabet: \n");
+    scanf(" %c", &ch);
+    
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')  {
+		printf("\n %c is a VOWEL.", ch);
+	}
+    else {
+    	printf("\n %c is a CONSONANT.", ch);
+    }
+}
