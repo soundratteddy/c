2c97e063fa2610822919981b52d33ea860d2731b
ANANDKUMAR594 <anandkumar.m594@gmail.com>
2017-08-13 08:36:08 +0530

    Postive/negative
    Checking whether it is positive or not

---
+++ postiveornot.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the value");
+scanf("%d",&a);
+if(a>0)
+printf("\nThe given integer is positive");
+else if(a<0)
+printf("\nThe given integer is negetive");
+else
+printf("\nThe given integer is neither positive nor negative");
+}
+
