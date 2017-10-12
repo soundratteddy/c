d30f0f0c3403670bea2451fbb420bd9af4bab718
ANANDKUMAR594 <anandkumar.m594@gmail.com>
2017-10-12 20:49:20 +0530

    leap year

---
+++ leap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int in;
+printf("Enter the year");
+scanf("%d",&in);
+if(in%4==0)
+{
+printf("\n The given year is a leap year");
+}
+else 
+printf("The given year is not a leap year");
+}
